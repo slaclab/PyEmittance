a451671b7e05468b3214a76ec7353f6dbe28614f
Sara A. Miskovich <saramiskovich@gmail.com>
2022-02-09 19:05:47 -0800

    new env file for emit calc only

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setup(
     long_description_content_type='text/markdown',
     install_requires=requirements,
     include_package_data=True,
-    python_requires='>=3.5'
+    python_requires='>=3.7'
 )
